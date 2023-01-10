e26b70d23005e5b754f306628b32cf3d3ba8a7d3
Charles Silva <42080053+charleshgps@users.noreply.github.com>
2023-01-10 16:53:48 -0300

    .

---
+++ 02_projeto_vue/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <FirstComponent />
+</template>
+
+<script>
+import FirstComponent from "./components/FirstComponent.vue";
+export default {
+  name: "App",
+  components: {
+    FirstComponent,
+  },
+};
+</script>
---
+++ 02_projeto_vue/src/components/FirstComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Ola Vue!!</h1>
+</template>
+
+<script>
+export default {
+  name: "FirstComponent",
+};
+</script>

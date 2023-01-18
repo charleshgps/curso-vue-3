ad2b70bbc776aaad4b9b13ab836932f745e7c9b7
Charles Silva <42080053+charleshgps@users.noreply.github.com>
2023-01-18 14:29:01 -0300

    .

--- 02_projeto_vue/src/components/Info.vue
+++ 02_projeto_vue/src/components/Info.vue
@@ -1,17 +1,37 @@
 <template>
   <div>
-    <p>Estou trabalhando no momento.</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidades</p>
     <p>Utilizo as seguintes linguagens</p>
     <ul>
       <li>Javascript</li>
       <li>Typescript</li>
       <li>Vue</li>
     </ul>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+    <p>
+      Para acessar o meu portifólio
+      <a v-bind:href="meu_link">Basta clicar aqui</a>
+    </p>
+    <Picture/>
   </div>
 </template>
 
 <script>
+
+import Picture from './Picture.vue';
 export default {
   name: "info",
+  components:{
+    Picture
+  }
+  data() {
+    return {
+      esta_trabalhando: true,
+      mostrar_email: true,
+      email: "charles_humberto@hotmail.com.br",
+      meu_link: "https://portfolio-charles-silva.netlify.app/",
+    };
+  },
 };
 </script>
---
+++ 02_projeto_vue/src/components/Picture.vue
@@ -0,0 +1,14 @@
+<template>
+  <img :src="avatar" :alt="descricao" />
+</template>
+<script>
+export default {
+  name: Picture,
+  data() {
+    return {
+      avatar: "/img/avata.png",
+      descricao: "Charles Humberto",
+    };
+  },
+};
+</script>
